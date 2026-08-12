592d6e50189890998537ab3de7ced635f56e1702
mtecfix <116046821+mtecfix@users.noreply.github.com>
2026-08-11 21:45:45 -0400

    Add source files

---
+++ PetLogger/Config.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Config {
+    static let apiEndpoint   = "https://tg5fkkc7k2.execute-api.us-east-1.amazonaws.com/dev"
+    static let cognitoPoolId = "us-east-1_epf05nztC"
+    static let cognitoClient = "2cbmo12ckk4i92b6jm2r80dq1i"
+    static let awsRegion     = "us-east-1"
+    static let s3Bucket      = "pet-logger-663877906756"
+}
