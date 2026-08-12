38ae4d087c771fa505306c376b3aac77189b35a4
mtecfix <116046821+mtecfix@users.noreply.github.com>
2026-08-12 17:55:53 -0400

    Add PetLoggerApp.swift

--- PetLogger/PetLoggerApp.swift
+++ PetLogger/PetLoggerApp.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 @main
 struct PetLoggerApp: App {
+    @StateObject private var notif = NotificationManager.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    Task { await notif.requestPermission() }
+                }
         }
     }
 }
